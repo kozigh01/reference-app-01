fc9ac63f8ce431ae32aac1bec462dca07692d824
kozigh01 <mkozi.gh01@gmail.com>
2021-03-14 02:18:01 +0000

    27:10 - add variable

---
+++ java/JavaTutorialForBeginners/HelloWorld/src/com/mkozi/Main.java
@@ -0,0 +1,11 @@
+package com.mkozi;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int age = 30;
+
+        
+        System.out.println("Hello, World!");
+        System.err.println("I'm on err");
+    }
+}
