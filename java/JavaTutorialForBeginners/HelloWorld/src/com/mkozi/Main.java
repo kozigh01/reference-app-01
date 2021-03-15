8bb88ce51fe7b9efa37e549bc37490c426eb6692
kozigh01 <mkozi.gh01@gmail.com>
2021-03-15 22:22:04 +0000

    java-1: time 1:25:52

--- java/JavaTutorialForBeginners/HelloWorld/src/com/mkozi/Main.java
+++ java/JavaTutorialForBeginners/HelloWorld/src/com/mkozi/Main.java
@@ -1,11 +1,68 @@
 package com.mkozi;
 
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
+class Bird {
+    public Bird() {
+
+    }
+
+    public void move() {
+        System.out.println("The bird flies away");
+    }
+}
+
+class Penguin extends Bird {
+    public Penguin() {
+
+    }
+
+    @Override
+    public void move() {
+        System.out.println("The bird waddles away");
+    }
+}
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        int age = 30;
+        Penguin myBird = new Penguin();
+        myBird.move();
 
+        Date now = new Date();
+        System.out.println(now);
         
-        System.out.println("Hello, World!");
-        System.err.println("I'm on err");
+        String message = "howdy";     
+        
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[2] = 2;
+        System.out.println(Arrays.toString(numbers));
+
+        int[] numbers2 = {5,4,3,2,1};
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers2));
+        Arrays.sort(numbers2);
+        System.out.println(Arrays.toString(numbers2));
+
+        int[][] numbers3 = new int[2][3];
+        numbers3[0][1] = 1;
+        System.out.println(Arrays.deepToString(numbers3));
+
+        int[][] numbers4 = { {1,2,3}, {4,5,6}};
+        System.out.println(Arrays.deepToString(numbers4));
+
+        // constants
+        final float PI = (float) 3.14;
+
+        NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.US);
+        String result = currency.format(123345.689);
+        System.out.println(result);
+
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        String result2 = percent.format(0.145);
+        System.out.println(result2);
     }
 }
---
+++ java/JavaTutorialForBeginners/HelloWorld/src/com/mkozi/Other.java
@@ -0,0 +1,11 @@
+package com.mkozi;
+
+public class Other {
+  public static void main(String[] args) {
+    Penguin private1 = new Penguin();
+    System.out.println(private1);
+    private1.move();
+
+    System.out.println("i'm in Other");
+  }
+}
